0b5ef402bc3442625b2e81eac67f3f2c22ef6de6
Jericho Siahaya <jericho@ultimagz.com>
2020-10-01 12:59:44 +0900

    Delete delete.php

--- crudjson/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-$delete_id = $_GET['delete_id'];
-$data = file_get_contents('users.json');
-$data = json_decode($data, true);
-unset($data[$delete_id]);
-//encode back to json
-$data = json_encode($data, JSON_PRETTY_PRINT);
-file_put_contents('users.json', $data);
-header('location: index.php');
